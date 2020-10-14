aa4306403fa17db5c9ad5cc358acba9d2a6aa73b
Betty Suravech <bsuravech@gmail.com>
2020-10-14 12:11:21 +0200

    build for current adminBundle only

--- indexAdmin.js
+++ indexAdmin.js
@@ -3,12 +3,11 @@ module.exports = {
   // we ask for the non-builded version of instantsearch.js to avoid
   // duplication of algoliasearch, algoliasearchHelper and hogan modules
   // that's why we also need some webpack configuration and modules here
-  instantsearch: require('instantsearch.js/dist/instantsearch.production.min.js'),
+  instantsearch: require('instantsearch.js/dist-es5-module'),
   algoliasearch: require('algoliasearch'),
   algoliasearchHelper: require('algoliasearch-helper'),
   Hogan: require('hogan.js'),
-  autocomplete: require('autocomplete.js/index_jquery.js'),
-  angular: require('angular')
+  autocomplete: require('autocomplete.js/index_jquery.js')
 };
 
 require('jquery-ui/sortable');
